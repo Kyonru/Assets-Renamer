37f4ddb473008021f0d5dd77c4b8b798577ff7a0
Roberto Amarante <r.amarante@gbh.com.do>
2018-03-21 13:38:14 -0400

    tests proving to fail

--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 import commands
 a = commands.getstatusoutput('ls -l | grep ^- | wc -l')
 
-if (int(a[1]) == 6):
+if (int(a[1]) == 10):
     print 'Nice job!'
 else:
     raise Exception('Please, try again')
